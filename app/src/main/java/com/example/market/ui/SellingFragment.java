2b931c85bd1ec115c3ef477d1df269b002ccf26f
55selcukozdemir <55selcukozdemir@gmail.com>
2021-09-21 20:10:50 +0300

    Bottom sheet in recyclerView Added

--- app/src/main/java/com/example/market/ui/SellingFragment.java
+++ app/src/main/java/com/example/market/ui/SellingFragment.java
@@ -3,6 +3,8 @@ package com.example.market.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +13,12 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.example.market.R;
+import com.example.market.adapter.SaleInEndAdapter;
 import com.example.market.databinding.FragmentSellingBinding;
+import com.example.market.product.SaleInEnd;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.util.ArrayList;
 
 
 public class SellingFragment extends Fragment {
@@ -24,15 +30,23 @@ public class SellingFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-
-
-
         binding = FragmentSellingBinding.inflate(getLayoutInflater());
 
+        //Fragment içine BottomSheet eklenmesi
         final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(container.getContext());
         bottomSheetDialog.setContentView(R.layout.bottomshet);
         bottomSheetDialog.show();
+        //Fragment içine BottomSheet eklenmesi (son)
+
+
+        //Bottom Sheet içine RecyclerView eklenmesi
+        ArrayList<SaleInEnd> saleArrayList = new ArrayList<>();
+        saleArrayList.add(new SaleInEnd("yeni","eski","devam","tamam"));
+        RecyclerView mRecyclerView = bottomSheetDialog.findViewById(R.id.bottomSheetRecyclerView);
+        SaleInEndAdapter saleInEndAdapter = new SaleInEndAdapter(saleArrayList,container.getContext());
+        mRecyclerView.setAdapter(saleInEndAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(container.getContext()));
+        ////Bottom Sheet içine RecyclerView eklenmesi (son)
 
         return binding.getRoot();
     }
