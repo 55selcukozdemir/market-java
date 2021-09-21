63c902225e2343d15a3c2103dbba185493d57990
55selcukozdemir <55selcukozdemir@gmail.com>
2021-09-21 08:41:56 +0300

    Bottom Sheet addad

--- app/src/main/java/com/example/market/ui/SellingFragment.java
+++ app/src/main/java/com/example/market/ui/SellingFragment.java
@@ -7,20 +7,34 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.example.market.R;
 import com.example.market.databinding.FragmentSellingBinding;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 
 public class SellingFragment extends Fragment {
 
     public FragmentSellingBinding binding;
 
+    public LinearLayout linearLayout;
+    public Button button;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+
+
+
         binding = FragmentSellingBinding.inflate(getLayoutInflater());
+
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(container.getContext());
+        bottomSheetDialog.setContentView(R.layout.bottomshet);
+        bottomSheetDialog.show();
 
         return binding.getRoot();
     }
+
 }
